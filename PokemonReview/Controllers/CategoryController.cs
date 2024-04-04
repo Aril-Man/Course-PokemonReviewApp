829f4fd86fa5597609763eef82ea2e34079e3841
Aril-Man <azrielfhr23@gmail.com>
2024-04-05 06:54:40 +0700

    update : add category and country api

---
+++ PokemonReview/Controllers/CategoryController.cs
@@ -0,0 +1,71 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PokemonReview.Dto;
+using PokemonReview.Interfaces;
+using PokemonReview.Models;
+
+namespace PokemonReview.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryRepository _repository;
+        private readonly IMapper _mapper;
+
+        public CategoryController(ICategoryRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Category>))]
+        public IActionResult GetCategories()
+        {
+            var categories = _mapper.Map<List<CategoryDto>>(_repository.GetCategories());
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return Ok(categories);
+        }
+
+        [HttpGet("{categoryId}")]
+        [ProducesResponseType(200, Type = typeof(Category))]
+        [ProducesResponseType(400)]
+        public IActionResult GetCategory(int categoryId)
+        {
+            if (!_repository.CategoriesExists(categoryId))
+            {
+                return NotFound();
+            }
+
+            var category = _mapper.Map<CategoryDto>(_repository.GetCategory(categoryId));
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return Ok(category);
+        }
+
+        [HttpGet("pokemon/{categoryId}")]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Pokemon>))]
+        [ProducesResponseType(400)]
+        public IActionResult GetPokemonByCategoryId(int categoryId)
+        {
+            var pokemons = _mapper.Map<List<PokemonDto>>(_repository.GetPokemonByCategory(categoryId));
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return Ok(pokemons);
+        }
+    }
+}
---
+++ PokemonReview/Controllers/CountryController.cs
@@ -0,0 +1,72 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PokemonReview.Dto;
+using PokemonReview.Interfaces;
+using PokemonReview.Models;
+
+namespace PokemonReview.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CountryController : Controller
+    {
+        private readonly ICountryRepository _repository;
+        private readonly IMapper _mapper;
+
+        public CountryController(ICountryRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Country>))]
+        public IActionResult GetCountries()
+        {
+            var countries = _mapper.Map<List<CountryDto>>(_repository.GetCountries());
+
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return Ok(countries);
+        }
+
+        [HttpGet("{countryId}")]
+        [ProducesResponseType(200, Type = typeof(Country))]
+        [ProducesResponseType(400)]
+        public IActionResult GetCountry(int countryId)
+        {
+            if (!_repository.CountryExists(countryId))
+            {
+                return NotFound();
+            }
+
+            var country = _mapper.Map<CountryDto>(_repository.GetCountry(countryId));
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return Ok(country);
+        }
+
+        [HttpGet("/owners/{ownerId}")]
+        [ProducesResponseType(200, Type = typeof(Country))]
+        [ProducesResponseType(400)]
+        public IActionResult GetCountryOfAnOwner(int ownerId)
+        {
+            var country = _mapper.Map<CountryDto>(_repository.GetCountryByOwner(ownerId));
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return Ok(country);
+        }
+    }
+}
